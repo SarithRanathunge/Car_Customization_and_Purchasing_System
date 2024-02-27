ad863a213b053172e362fce855f615fc49297b8f
Rsarith <130320141+SarithRanathunge@users.noreply.github.com>
2024-02-27 23:31:03 +0530

    Create a singly linked list
    Create a singly linked list and node

---
+++ src/singlyLinkedList/LinkedListNode.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package singlyLinkedList;
+
+import nodeArray.ArrayNode;
+
+/**
+ *
+ * @author Sarith
+ */
+public class LinkedListNode {
+    public String fullName;
+    public String NIC;
+    public String telephone;
+    public String email;
+    public String address;
+    public String data;
+    public ArrayNode[] array;
+    public int arraySize;
+    public LinkedListNode link;
+
+    public LinkedListNode(String fullName, String NIC, String telephone, String email, String address, String data, ArrayNode[] array) {
+        this.fullName = fullName;
+        this.NIC = NIC;
+        this.telephone = telephone;
+        this.email = email;
+        this.address = address;
+        this.data = data;
+        this.array = array;
+        link = null;
+    }
+}
---
+++ src/singlyLinkedList/SinglyLinkedList.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package singlyLinkedList;
+import singlyLinkedList.LinkedListNode;
+import java.lang.String;
+import nodeArray.ArrayNode;
+/**
+ *
+ * @author Sarith
+ */
+public class SinglyLinkedList {
+    public LinkedListNode head;
+
+    public SinglyLinkedList(){
+        head = null;
+    }
+    
+    public void insertAtBack(String fullName, String NIC, String telephone, String email, String address, String data, ArrayNode[] array){
+        LinkedListNode newNode = new LinkedListNode( fullName, NIC, telephone, email, address, data, array);
+        if(isEmpty()){
+            head = newNode;
+        }else{
+            LinkedListNode tempNode = head;
+            while(tempNode.link != null){
+                tempNode = tempNode.link;
+            }
+            tempNode.link = newNode;
+        }
+    }
+    
+    public void display(){
+        if(isEmpty()){
+            System.out.println("LinkedList is empty.");
+        }else{
+            LinkedListNode tempNode = head;
+            System.out.println(tempNode.fullName);
+        }
+    }
+    
+    public boolean isEmpty(){
+        return (head == null);
+    }
+}
