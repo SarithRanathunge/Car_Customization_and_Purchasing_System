ddbce3262e1286064b4c89dc54cb747845c5cf78
Rsarith <130320141+SarithRanathunge@users.noreply.github.com>
2024-02-27 17:16:56 +0530

    Create a array node class
    Create a array node class in a node array package

---
+++ src/nodeArray/ArrayNode.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nodeArray;
+
+/**
+ *
+ * @author Sarith
+ */
+public class ArrayNode {
+
+    public String item;
+    public double price;
+
+    public ArrayNode(String item, double price) {
+        this.item = item;
+        this.price = price;
+    }
+}
